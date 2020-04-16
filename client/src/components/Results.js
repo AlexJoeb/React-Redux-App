ecbfb42653c47eb34ec26dbd8c0f39b85213438d
Alexander Besse <alexanderjoeb@hotmail.com>
2020-04-16 13:29:42 -0500

    Polish Results

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Controls from './Controls'
 
+import { Link } from 'react-router-dom';
+
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 
-import { Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@material-ui/core";
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, Button } from "@material-ui/core";
  
 export function Results({ results }) {
     return (
@@ -14,24 +16,42 @@ export function Results({ results }) {
             <ul css={css`
                 list-style: none;
                 font-family: sans-serif;
+
+                display: flex;
+                flex-wrap: wrap;
+                justify-content:space-around;
+                align-items: center;
             `}>
                 {
                     results.length > 0 ? results.map((item, index) => {
-                        console.log(item)
+                        console.log(item.href)
                         return (
-                            <li key={index}>
+                            <li css={css`flex-basis: 48%; margin-bottom: 20px;`} key={index}>
                                 <Card>
-                                    <CardActionArea>
-                                        <CardMedia image={item.thumbnail || ""} title={item.title} />
+                                    <CardActionArea css={css`display:flex;flex-direction:column;`}>
+                                        {item.thumbnail.trim() && <CardMedia component='img' alt={item.title} image={item.thumbnail} title={item.title} />}
+                                        <CardContent css={css`flex: 1;`}>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {item.title}
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary" component="p">
+                                                {item.ingredients}
+                                            </Typography>
+                                        </CardContent>
                                     </CardActionArea>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {item.title}
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                            {item.ingredients}
-                                        </Typography>
-                                    </CardContent>
+                                    <CardActions>
+                                        <a css={css`
+                                            color: gray;
+                                            font-size: .8rem;
+                                            margin-left: 10px;
+
+                                            &:hover{
+                                                color:darkgray;
+                                            }
+                                        `} href={item.href}>
+                                            See full recipe.
+                                        </a>
+                                    </CardActions>
                                 </Card>
                             </li>
                         )
